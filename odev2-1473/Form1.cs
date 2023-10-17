1d5f0e077b1aff74d7cf2403db1b971b8aaf89ff
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2023-10-17 14:12:52 +0300

    Add files via upload

---
+++ odev2-1473/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev2_1473
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string secim;
+            secim = listBox1.SelectedItems.ToString();
+            if(secim == "mouse"|| secim == "klavye" || secim == "kamera" || secim == "tarayıcı")
+            {
+                MessageBox.Show("bu parça,giriş birimindir.");
+            }
+            if (secim == "yazıcı" || secim == "hoparlör" || secim == "projeksiyon")
+            {
+                MessageBox.Show("bu parça, çıkış birimindir.");
+            }
+        }
+    }
+}
